0fa872c3587a5cf10f617585f6d113fd7bd9f46c
Tringapps-Dharshan <102800724+Tringapps-Dharshan@users.noreply.github.com>
2022-04-27 11:39:15 +0530

    update

---
+++ index.js
@@ -0,0 +1,9 @@
+function fb(){
+    document.getElementById('current').innerHTML="Facebook";
+}
+function yt(){
+    document.getElementById('current').innerHTML="YouTube";
+}
+function gp(){
+    document.getElementById('current').innerHTML="GooglePlus";
+}
